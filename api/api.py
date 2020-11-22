60b97c5051b1d79f5a24dd61d998db3c47c001ce
Aleksandr Vodyakov <4ingers@gmail.com>
2020-11-22 12:05:54 +0300

    Добавлена навигация по лабораторным

--- api/api.py
+++ api/api.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+help_list = {
+  '1': 'Help for the first lab',
+  '2': 'Help for the second lab',
+}
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Ccfsdfhicken McNuggets are available on other pages...'
+  return 'Chicken McNuggets stuff is available on other pages...'
+
+@app.route('/labs')
+def help():
+  return help_list
+
+@app.route('/labs/<string:id>', methods=['POST'])
+def execute(id):
+  content = request.json
+  # return {"id": id}
+  return content
--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Labs from './components/Labs'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+import axios from 'axios'
 
 function App() {
+  let imageClick = e => {
+    axios.post('/labs/1', { 'abc': '123' })
+      // axios.get('/labs')
+      .then(res => res.data)
+      .then(data => console.log(data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edgjksdjkghit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Labs />
   );
 }
 
